a480e5425ce8e8bdc27b736594c8af65af604127
manuchandra2812-lang <manuchandra2812@gmail.com>
2026-06-03 17:57:27 +0530

    Day 1 - Created canvas and display function
    Added a simple 2D graphics editor with canvas initialization and display functions.

--- main.c
+++ main.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+#define HEIGHT 20
+#define WIDTH 40
+
+char canvas[HEIGHT][WIDTH];
+
+void initializeCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < HEIGHT; i++)
+    {
+        for(j = 0; j < WIDTH; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < HEIGHT; i++)
+    {
+        for(j = 0; j < WIDTH; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    printf("===== 2D GRAPHICS EDITOR =====\n\n");
+
+    initializeCanvas();
+    displayCanvas();
+
+    return 0;
+}
