e1569d7fba61db02b1f03ab610f2869f7d4a4862
Manoj L E <manuchandra2812@gmail.com>
2026-06-07 15:43:59 +0530

    Day 1: The Engine & Basic Canvas
    Set up the basic data structures, the 2D canvas, the menu loop, and implement the fundamental line-drawing algorithm.
    Suggested GitHub Commit Message: Initial commit: Setup canvas, basic structures, and line drawing feature.

--- main.c
+++ main.c
@@ -1,85 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define WIDTH 60
 #define HEIGHT 20
-#define WIDTH 40
-int main()
-{
-    printf("===== 2D GRAPHICS EDITOR =====\n\n");
+#define MAX_OBJECTS 100
 
-    initializeCanvas();
+typedef enum { LINE } ShapeType;
 
-    drawLine(5, 10, 30);
-
-    displayCanvas();
-
-    return 0;
-}
+typedef struct {
+    int id;
+    ShapeType type;
+    int x1, y1, x2, y2; 
+    int active;
+} GraphicsObject;
 
 char canvas[HEIGHT][WIDTH];
+GraphicsObject objects[MAX_OBJECTS];
+int objectCount = 0;
+int nextId = 1;
 
-void initializeCanvas()
-{
-    int i, j;
-
-    for(i = 0; i < HEIGHT; i++)
-    {
-        for(j = 0; j < WIDTH; j++)
-        {
+void clearCanvas() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
             canvas[i][j] = '_';
         }
     }
 }
 
-void drawLine(int row, int startCol, int endCol)
-{
-    int j;
-
-    for(j = startCol; j <= endCol; j++)
-    {
-        canvas[row][j] = '*';
-    }
-}
-
-void displayCanvas()
-{
-    int i, j;
-
-    for(i = 0; i < HEIGHT; i++)
-    {
-        for(j = 0; j < WIDTH; j++)
-        {
-            printf("%c", canvas[i][j]);
+void displayCanvas() {
+    printf("\n--- Current Canvas ---\n");
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c ", canvas[i][j]);
         }
         printf("\n");
     }
+    printf("----------------------\n");
 }
-void drawRectangle(int row, int col, int height, int width)
-{
-    int i, j;
 
-    for(j = col; j < col + width; j++)
-    {
-        canvas[row][j] = '*';
-        canvas[row + height - 1][j] = '*';
-    }
-
-    for(i = row; i < row + height; i++)
-    {
-        canvas[i][col] = '*';
-        canvas[i][col + width - 1] = '*';
+void plotPoint(int x, int y) {
+    if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+        canvas[y][x] = '*';
     }
 }
 
-void drawTriangle(int row, int col, int height)
-{
-    int i, j;
-    for(i = 0; i < height; i++)
-    {
-        for(j = 0; j <= i; j++)
-        {
-            canvas[row + i][col + j] = '*';
+void drawLine(int x1, int y1, int x2, int y2) {
+    int dx = abs(x2 - x1), sx = x1 < x2 ? 1 : -1;
+    int dy = -abs(y2 - y1), sy = y1 < y2 ? 1 : -1;
+    int err = dx + dy, e2;
+
+    while (1) {
+        plotPoint(x1, y1);
+        if (x1 == x2 && y1 == y2) break;
+        e2 = 2 * err;
+        if (e2 >= dy) { err += dy; x1 += sx; }
+        if (e2 <= dx) { err += dx; y1 += sy; }
+    }
+}
+
+void renderAll() {
+    clearCanvas();
+    for (int i = 0; i < objectCount; i++) {
+        if (objects[i].active) {
+            if (objects[i].type == LINE) {
+                drawLine(objects[i].x1, objects[i].y1, objects[i].x2, objects[i].y2);
+            }
         }
     }
 }
 
+void addObject() {
+    if (objectCount >= MAX_OBJECTS) return;
+    
+    GraphicsObject obj;
+    obj.id = nextId++;
+    obj.active = 1;
+    obj.type = LINE;
+    
+    printf("Add Line - Enter x1 y1 x2 y2: ");
+    scanf("%d %d %d %d", &obj.x1, &obj.y1, &obj.x2, &obj.y2);
+    
+    objects[objectCount++] = obj;
+    renderAll();
+}
 
+int main() {
+    clearCanvas();
+    int choice;
+
+    do {
+        displayCanvas();
+        printf("\n1. Add Line\n2. Exit\nEnter choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) addObject();
+    } while (choice != 2);
+
+    return 0;
+}
